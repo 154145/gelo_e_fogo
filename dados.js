d13a64178e78dbc502668e3ff57996dbeee6e1ac
154145 <162905091+154145@users.noreply.github.com>
2024-09-06 12:14:11 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+      titulo: "Tyrion Lannister",
+      descricao: "Tyrion Lannister, o anão da Casa Lannister, é uma figura complexa e fascinante no universo de Game of Thrones. Nascido em uma família nobre e poderosa, sua deformidade física o torna um pária aos olhos de seu pai e de muitos outros. No entanto, sua inteligência aguçada, seu senso de humor irônico e sua capacidade de adaptação o tornam um estrategista brilhante e um negociador hábil. Apesar de sua condição, Tyrion busca constantemente provar seu valor e encontrar seu lugar no mundo, enfrentando desafios e traições ao longo de sua jornada. Sua história é marcada por momentos de grande sofrimento e momentos de glória, tornando-o um dos personagens mais memoráveis e controversos da saga.",
+      link: "https://gameofthrones.fandom.com/pt-br/wiki/Tyrion_Lannister",
+      tags:"anão duende fatricida"
+    },
+    {
+      titulo: "Daenerys Targaryen",
+      descricao: "Daenerys Targaryen, a Mãe dos Dragões, é uma das personagens mais poderosas e icônicas de Game of Thrones. Exilada desde a infância, ela cresce em um mundo hostil e luta para reconquistar o Trono de Ferro, que acredita ser seu por direito. Com a ajuda de seus três dragões, Daenerys se torna uma líder carismática e inspiradora, unindo exércitos e conquistando cidades. No entanto, a busca pelo poder a leva por um caminho obscuro, e ela precisa enfrentar decisões difíceis que desafiam sua humanidade.",
+      link: "https://gameofthrones.fandom.com/pt-br/wiki/Daenerys_Targaryen",
+      tags: "mãe dragões rainha"
+    },
+    {
+      titulo: "Jon Snow",
+      descricao: "Jon Snow, o bastardo de Ned Stark, é um personagem complexo e enigmático. Criado no Norte, ele se junta à Patrulha da Noite para proteger os Sete Reinos dos perigos além da Muralha. Descobre sua verdadeira identidade como Targaryen e se torna uma figura central na guerra contra os White Walkers. Jon Snow é um líder nato, justo e leal, mas também um homem cheio de dúvidas e dividido entre seus deveres e seus sentimentos.",
+      link: "https://gameofthrones.fandom.com/pt-br/wiki/Jon_Snow",
+      tags: "bastardo corvo lorde"
+    }
+  ];
